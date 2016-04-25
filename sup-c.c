008f0e2ec72ae1b01c2656f632183f900b1a7374
Piotr Walczak <P.Walczak@adbglobal.com>
2016-04-25 13:47:42 +0200

    adding foo_bar feature

--- sup-c.c
+++ sup-c.c
@@ -2,16 +2,24 @@
 #include <stdlib.h>
 
 void hello_yocto(void);
+int foo_bar(void);
 
 int main(int argc, char **argv)
 {
     printf("\nIn %s calling function: hello_yocto\n", __FUNCTION__);
     hello_yocto();
+    foo_bar();
+    printf("Exiting...\n\n");
     return 0;
 }
 
 void hello_yocto(void)
 {
     printf("\nHello Yocto\n");
-    printf("devtool test on poky 2.0.1\n\n");
+    printf("devtool test on poky 2.0.1\n");
+}
+
+int foo_bar(void)
+{
+    return 0;
 }
